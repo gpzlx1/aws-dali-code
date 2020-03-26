33c9f56e7d7077c4b35d08b8c9bee45bd9e9cad8
gpzlx1 <gpzlx1@mail.ustc.edu.cn>
2020-03-26 14:38:23 +0800

    updata

--- run.sh
+++ run.sh
@@ -20,12 +20,12 @@ batch_size=512
 
 for num_worker in 4 5 6 7 8 9 10 11 12 13
 do
-for arch in alexnet resnet18 shufflenet_v2_x1_0
+for arch in alexnet  
 do
 nohup nvidia-smi dmon -s pucvmet -c 500 -f result/alexnet_cpu_decode/${arch}_${num_worker}.gpu &
 /home/ubuntu/nmon -s 1 -c 500 -F result/alexnet_cpu_decode/${arch}_${num_worker}.nmon
 #sudo sync &&  echo 3 > /proc/sys/vm/drop_caches
-$python_path -m torch.distributed.launch --nproc_per_node=$num_gpu ${src_path}dali_tfr_cpu.py --epochs $num_epoch  --workers $num_worker   -a $arch -b $batch_size --fp16  $record_path > result/alexnet_cpu_decode/${arch}_${num_worker}.data
+$python_path -m torch.distributed.launch --nproc_per_node=$num_gpu ${src_path}dali_tfr1.py --epochs $num_epoch  --workers $num_worker   -a $arch -b $batch_size --fp16  $record_path > result/alexnet_cpu_decode/${arch}_${num_worker}.data
 pkill python3
 pkill nmon
 pkill nvidia-smi
